f3452a00d621b8d9bc929400bfb0b5e2c99061ce
ASHUTOSH CHAUHAN <123000845+ashu034@users.noreply.github.com>
2026-01-29 20:26:08 +0530

    Add files via upload

---
+++ src/main/java/com/touragency/touragency_backend/TouragencyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.touragency.touragency_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouragencyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouragencyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/touragency/touragency_backend/TouragencyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.touragency.touragency_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouragencyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
